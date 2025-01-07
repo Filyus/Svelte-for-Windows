7c555d2023006a34b182ffdc94deb5c8a0502550
Filyus <fahmedyanov@dpg.email>
2025-01-08 01:38:02 +0500

    feat: auto-start a browser

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,37 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { str, port, bool, cleanEnv } from 'envalid'
+
+const baseEnv = cleanEnv(process.env, {
+  PORT: port({ default: 5173 }),
+  OPEN: bool({ default: true }),
+  BROWSER: str({ default: 'msedge' })
+});
+
+const derivedEnv = cleanEnv(process.env, {
+  BROWSER_ARGS: str({
+    default: [
+      '--app=http://localhost:' + baseEnv.PORT
+    ].join(' ')
+  })
+});
+
+const env = {
+  ...baseEnv,
+  ...derivedEnv
+};
+
+for (const [key, value] of Object.entries(env) as [keyof typeof env, any][]) {
+  if (
+    key !== 'isDevelopment' &&
+    key !== 'isDev' &&
+    key !== 'isProduction' &&
+    key !== 'isProd' &&
+    key !== 'isTest'
+  ) {
+    process.env[key] = String(value);
+  }
+}
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -24,7 +56,13 @@ export default defineConfig({
   server: {
     fs: {
       strict: false  // Allow serving files from outside the root directory
-    }
+    },
+    port: env.PORT,
+    open: env.OPEN ? `http://localhost:${env.PORT}` : false
+  },
+  preview: {
+    port: env.PORT,
+    open: env.OPEN ? `http://localhost:${env.PORT}` : false
   },
   resolve: {
     extensions: ['.js', '.ts', '.svelte']
